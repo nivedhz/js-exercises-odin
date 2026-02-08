b09a68dbfd05975dafdd905c492b00cf9dfa2ebb
Nivedh E R <nivedher12@gmail.com>
2026-02-08 09:53:41 +0000

    Process: If every element inside the ...num is not an integer or if any/some element inside ...num is a negative number return ERROR Else in a min variable get the smallest value in the ...num array and do the same with max In the noOfIntegers variable get the number of integers between max and min with the eqn max - mi+ 1 Now apply all this to a main eqn (Sum of arithmetic series formula) ie, (noOfIntegers*(max-min))/2 and return the result of the eqn

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(...num) {
+    if (!num.every(Number.isInteger) || num.some(x => x < 0)){
+        return "ERROR";
+    } 
+    else{
+        const min = Math.min(...num);
+        const max = Math.max(...num);
+        const noOfIntegers = max - min + 1;
+        return (noOfIntegers * (min + max)) / 2;
+    }
 };
 
 // Do not edit below this line
