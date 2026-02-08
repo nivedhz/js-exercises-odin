2bc1f9d0f1fac36cf6992a0d7078310fc70b62b9
Nivedh E R <nivedher12@gmail.com>
2026-02-08 09:04:32 +0000

    Process: The ...num packs every extra arguments inside them let removedArray = [] just creates an empty array for every element in array if the elemnt does not have the arguments/elements of num, then push the x into the empty removedArray and continue until no element of array is left

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...num) {
+    let removedArray = [];
+    for (let x of array){
+        if (!num.includes(x)){
+            removedArray.push(x)
+        }
+    }
+    return removedArray;
+    }
 
 // Do not edit below this line
 module.exports = removeFromArray;
